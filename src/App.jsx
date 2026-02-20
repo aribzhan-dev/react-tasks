be073033d08c92c4d54f1fc531d915a7aba0e7e9
Aribzhan <aribzankamilzanov@gmail.com>
2026-02-20 15:40:25 +0500

    new fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './Card'
-import StateExample  from './components/StateExample/StateExample'
-
-
-
-
-
-// const cardInfo = [
-//   {
-//     name: "Card 1",
-//     description: "This is the first card."
-//   },
-//   {
-//     name: "Card 2",
-//     description: "This is the second card."
-//   },
-//   {
-//     name: "Card 3",
-//     description: "This is the third card."
-//   },
-//   {
-//     name: "Card 4",
-//     description: "This is the fourth card."
-//   },
-//   {
-//     name: "Card 5",
-//     description: "This is the fifth card."
-//   }
-// ]
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import UserList from './pages/UserList';
+import Header from './components/Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
   return (
     <>
-     <StateExample></StateExample>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<UserList />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Counter/Counter.jsx
+++
@@ -1,7 +0,0 @@
-function Counter({ c }) {
-    return (
-        <h1>Count: {c}</h1>
-    )
-}
-
-export default Counter;
